c98fcd9f93782355d2fdc46393067acf2da3906e
sachinrajgire <sachin.rajgire@mgmail.com>
2022-04-14 21:33:32 -0400

    ready to dploy

--- src/About/Aboutus.js
+++ src/About/Aboutus.js
@@ -9,11 +9,11 @@ export default function ActionAreaCard() {
             About this Application
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            App created by <a href="https://wynisco.com" target="_blank">Wynisco</a> bootcamp students
+            App created by <a href="https://wynisco.com" target="_blank" rel="noreferrer">Wynisco</a> bootcamp students
           </Typography>
           <Typography variant="body2" color="text.secondary">
-           Content based on <a href="https://immigration.com" target="_blank">Immigration.com </a>
-           and <a href="https://immigration.com/sites/default/files/F-1%20Visa%20Holder%2C%20May%20you%20Travel%20%28OPTH-1B%20Situations%29_0.png" target="_blank">this</a> flow chart
+           Content based on <a href="https://immigration.com" target="_blank" rel="noreferrer">Immigration.com </a>
+           and <a href="https://immigration.com/sites/default/files/F-1%20Visa%20Holder%2C%20May%20you%20Travel%20%28OPTH-1B%20Situations%29_0.png" target="_blank" rel="noreferrer">this</a> flow chart
           </Typography>
           </div>
   );
